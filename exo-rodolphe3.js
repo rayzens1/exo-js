881a9d1fe6111eaff041f233cda7fcb3f461fa11
rayzens1 <goldenguysofficial@gmail.com>
2025-02-12 10:26:07 +0100

    Done 2 exercice

---
+++ exo-rodolphe3.js
@@ -0,0 +1,5 @@
+const destination = ["Quillan", "Artigues", "Belvis", "Montjardin", "Escouloubre"];
+
+console.log(destination[0]);
+console.log(destination.length);
+console.log(String(destination));
---
+++ exo-rodolphe4.js
@@ -0,0 +1,11 @@
+const ville = {
+    name: "Artigues",
+    hotel: 4,
+    gites: 12,
+    randonnées: 9
+}
+
+console.log(ville.name);
+console.log(ville.hotel);
+console.log(ville.gites);
+console.log(ville.randonnées);
